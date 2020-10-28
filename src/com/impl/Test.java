5df58a4217a151a17cb8d0770c293f32bf9ac194
giobassi <giobassi@MIL-2P0G0N2.usersad.everis.int>
2020-10-28 01:33:08 +0100

    Terzo commit

--- src/com/impl/Test.java
+++ src/com/impl/Test.java
@@ -6,51 +6,123 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
+import javax.print.attribute.standard.DateTimeAtCompleted;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
+import javafx.util.converter.DateTimeStringConverter;
 
 
 public class Test {
 	public static void main(String[] args) {
+		System.out.println("---------------------------------------------------------------------------------------------------");
+		System.out.println("");
+		System.out.println("Saldo del conto corrente 14537780");
 		saldo("14537780");
+		System.out.println("");
+		System.out.println("---------------------------------------------------------------------------------------------------");
+		System.out.println("");
+		System.out.println("Movimenti del conto corrente 14537780 dal 2019-01-01 al 2019-12-01");
+		listaTransazioni("14537780","2019-01-01","2019-12-01");
+		System.out.println("");
+		System.out.println("---------------------------------------------------------------------------------------------------");
+		
+		
 		//transfers();
-		//listaTransazioni();
+		
 	}
 
 	public static void saldo(String accounts) {
-		try{
-			StringBuilder urlS = new StringBuilder("https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/");
-			urlS.append(accounts);
-			urlS.append("/balance");
-			URL url = new URL(urlS.toString());
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Auth-Schema", "S2S");
-            conn.setRequestProperty("Api-Key", "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP");
-            conn.setRequestProperty("X-Time-Zone", "Europe/Rome");
-            
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Errore : " + conn.getResponseCode());
-            }
-            
-            InputStreamReader in = new InputStreamReader(conn.getInputStream());
-            BufferedReader br = new BufferedReader(in);
-            StringBuilder risposta = new StringBuilder(""); 
-            String output;
-            while ((output = br.readLine()) != null) {
-            	risposta.append(output);
-            }
-            
-            JSONParser parse = new JSONParser();
-            JSONObject data_obj = (JSONObject) parse.parse(risposta.toString());
-            JSONObject obj = (JSONObject) data_obj.get("payload");
-            System.out.println("Saldo : " + obj.get("balance"));
-            
-            conn.disconnect();
-		} catch (Exception e) {
-			System.out.println("Error");
-			e.printStackTrace();
+		if (accounts != null && !accounts.isEmpty()) {
+			try {
+				StringBuilder urlS = new StringBuilder("https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/");
+				urlS.append(accounts);
+				urlS.append("/balance");
+				URL url = new URL(urlS.toString());
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Auth-Schema", "S2S");
+				conn.setRequestProperty("Api-Key", "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP");
+				conn.setRequestProperty("X-Time-Zone", "Europe/Rome");
+
+				if (conn.getResponseCode() != 200) {
+					throw new RuntimeException("Errore : " + conn.getResponseCode());
+				}
+
+				InputStreamReader in = new InputStreamReader(conn.getInputStream());
+				BufferedReader br = new BufferedReader(in);
+				StringBuilder risposta = new StringBuilder("");
+				String output;
+				while ((output = br.readLine()) != null) {
+					risposta.append(output);
+				}
+
+				JSONParser parse = new JSONParser();
+				JSONObject data_obj = (JSONObject) parse.parse(risposta.toString());
+				JSONObject obj = (JSONObject) data_obj.get("payload");
+				System.out.println("Saldo : " + obj.get("balance"));
+
+				conn.disconnect();
+			} catch (Exception e) {
+				System.out.println("Error");
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("Numero conto assente");
+		}
+	}
+	
+	public static void listaTransazioni(String accounts, String from, String to) {
+		if (accounts != null && !accounts.isEmpty() && from != null && !from.isEmpty() && to != null && !to.isEmpty()) {
+			try {
+				StringBuilder urlS = new StringBuilder("https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/");
+				urlS.append(accounts);
+				urlS.append("/transactions?fromAccountingDate=");
+				urlS.append(from);
+				urlS.append("&toAccountingDate=");
+				urlS.append(to);
+				URL url = new URL(urlS.toString());
+
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Auth-Schema", "S2S");
+				conn.setRequestProperty("Api-Key", "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP");
+				conn.setRequestProperty("X-Time-Zone", "Europe/Rome");
+
+				if (conn.getResponseCode() != 200) {
+					throw new RuntimeException("Errore : " + conn.getResponseCode());
+				}
+
+				InputStreamReader in = new InputStreamReader(conn.getInputStream());
+				BufferedReader br = new BufferedReader(in);
+				StringBuilder risposta = new StringBuilder("");
+				String output;
+				while ((output = br.readLine()) != null) {
+					risposta.append(output);
+				}
+
+				JSONParser parse = new JSONParser();
+				JSONObject data_obj = (JSONObject) parse.parse(risposta.toString());
+				JSONObject obj = (JSONObject) data_obj.get("payload");
+				JSONArray obj_list = (JSONArray) obj.get("list");
+				for (Object o : obj_list) {
+					if (o instanceof JSONObject) {
+						System.out.println("Movimento del : " + ((JSONObject) o).get("valueDate") + " di "
+								+ ((JSONObject) o).get("amount") + " " + ((JSONObject) o).get("currency"));
+					}
+				}
+
+				conn.disconnect();
+			} catch (Exception e) {
+				System.out.println("Error listaTransazioni");
+			}
 		}
 	}
 		
@@ -112,28 +184,5 @@ public class Test {
 		}
 	}
 	
-	public static void listaTransazioni() {
-		try{
-			URL url = new URL("https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/14537780/transactions?fromAccountingDate=2019-01-01&toAccountingDate=2019-12-01");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Auth-Schema", "S2S");
-            conn.setRequestProperty("Api-Key", "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP");
-            conn.setRequestProperty("X-Time-Zone", "Europe/Rome");
-                    
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP Error code : "
-                        + conn.getResponseCode());
-            }
-            InputStreamReader in = new InputStreamReader(conn.getInputStream());
-            BufferedReader br = new BufferedReader(in);
-            String output;
-            while ((output = br.readLine()) != null) {
-                System.out.println(output);
-            }
-            conn.disconnect();
-		}catch (Exception e) {
-			System.out.println("Error listaTransazioni");
-		}
-	}
+
 }
